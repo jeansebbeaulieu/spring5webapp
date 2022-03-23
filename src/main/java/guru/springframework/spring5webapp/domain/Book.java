edb0cdeb109c597e49c8a81b741d8bd4466d2090
Jean-Sébastien Beaulieu <jsbeaulieu87@gmail.com>
2022-03-23 15:37:15 -0400

    Added publisher relationships

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -11,7 +11,7 @@ public class Book {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(  Long id) {
         this.id = id;
     }
 
@@ -34,6 +34,17 @@ public class Book {
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
+
+    @ManyToOne
+    private Publisher publisher;
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
 
     public String getTitle() {
         return title;
